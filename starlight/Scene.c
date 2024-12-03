1b0c13fc5f9ee8478dbaed576f2fe21b608c6450
Steven <steven@mcclain.net>
2024-12-03 10:11:25 -0800

    Split off Scene.c and Scene.h

---
+++ starlight/Scene.c
@@ -0,0 +1,104 @@
+// File: Scene.c
+
+#include "Common.h"
+#include "Scene.h"
+
+#include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// //#include <time.h>
+
+// #include "obled.h"
+// #include "Queue.h"
+#include "Led.h"
+// #include "Morph.h"
+
+
+PRIVATE void Unpack_Scene(uint32_t val, uint8_t* buff)
+//
+// Extract scene bytes from uint32 value.
+{
+	buff[0] = val & 0xFF;
+	buff[1] = (val >> 8) & 0xFF;
+	buff[2] = (val >> 16) & 0xFF;
+	buff[3] = (val >> 24) & 0xFF;
+}
+
+
+PUBLIC void Render_Scene(uint32_t* start_ptr)
+//
+// Render scene given pointer.
+{
+	uint32_t* ptr = start_ptr;
+
+	uint32_t i = 0;
+
+	while (i < Num_LEDS)
+	{
+		uint8_t buff[4];
+		D(printf(": i = %d, ", i);)
+
+		Unpack_Scene(*ptr++, buff);
+
+		uint8_t flags = buff[0];
+
+		if (flags == FLAGS_END)
+		{
+			D(printf("End\n");)
+			break;
+		}
+		else if (flags == FLAGS_END_ALL)  
+		{
+			D(printf("End All\n");)
+			ptr = start_ptr;
+		}
+		else if (flags == FLAGS_END_LAST)
+		{
+			D(printf("End Last");)
+			while (i < Num_LEDS)
+			{
+				LED_Set_RGB(i, buff[1], buff[2], buff[3]);
+				++i;
+				D(printf(".");)
+			}
+			D(printf("\n");)
+		}
+		else if (flags == FLAGS_SKIP)
+		{
+			D(printf("skip %d\n", buff[1]);)
+				i += buff[1];
+		}
+		else
+		{
+			D(printf("%d %d %d", buff[1], buff[2], buff[3]);)
+			int count = flags ? flags : 1;
+			while (count--)
+			{
+				LED_Set_RGB(i, buff[1], buff[2], buff[3]);
+				++i;
+				D(printf(".");)
+			}
+			D(printf("\n");)
+		}
+	}
+	LED_Update();
+}
+
+
+PUBLIC void Set_Scene(SCENE_ID id)
+//
+// Render scene given scene id.
+{
+//	if (id && id <= Blob.Num_Scenes)
+	{
+		uint32_t start_idx = Blob.Scene_Index[--id];
+
+		D(printf("Set scene: id = %d, start_idx= %d\n", id, start_idx);)
+
+		Render_Scene(Blob.Scene_Array + start_idx);
+	}
+//	else { D(printf("Set scene: bad id = %d\n", id);) }
+}
+
+
+// EndFile: Scene.c
